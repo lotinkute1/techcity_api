b649f98c9af6d5ea7309e657775df4d1654d9933
Tín <mac@Macs-MacBook-Pro.local>
2022-03-14 19:50:00 +0700

    go go

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,10 @@
         </style>
     </head>
     <body class="antialiased">
+    <form action="{{route('paypal_call')}}" method="post">
+        @csrf
+        <button type="submit" class="btn btn-primary" >Place Order</button>
+        </form>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
